0f34a1be4f55f35a3ccedd9f2ad571a43f44ecc0
Sy Tri Ha <hatrisy@gmail.com>
2022-12-06 14:39:28 +1000

    update docker

--- client/src/helper.js
+++ client/src/helper.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const BASE_URL = "http://34.212.171.201:5000/api/";
+// const BASE_URL = "http://ecom-shop-api:5000/api/";
+const BASE_URL = process.env.BASE_URL;
 const TOKEN =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMTFkOTZlMTczNjhjYjc3YmM5NzBlMiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2MjYzMTc2MiwiZXhwIjoxNjYyODkwOTYyfQ.LXy0qhPMzUt9_7iFf2nxPmXkNFTBHMX07t9_jlY1pTs";
 export const publicRequest = axios.create({
